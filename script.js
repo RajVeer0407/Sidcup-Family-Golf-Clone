caf1c85acbfb6044cf5faa24009e3c57a0566f2b
RajVeer0407 <er.rajveershaw@gmail.com>
2024-06-17 17:33:55 +0530

    Completed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var crsr = document.querySelector("#cursor")
 var blur = document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+25+"px"
     crsr.style.top = dets.y+"px"
     blur.style.left = dets.x - 250 + "px"
     blur.style.top = dets.y - 250 + "px"
@@ -78,3 +78,41 @@ gsap.from(".card",{
         scrub:1
     }
 })
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+      // markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
